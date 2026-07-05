2bda1a93020b3767a0e23ac74bf604b8f872ff2c
Fatima ezzahra Lamlih <fatimaezzahralamlih776@gmail.com>
2026-07-05 13:12:03 +0100

    advance error handling

--- src/__main__.py
+++ src/__main__.py
@@ -5,17 +5,19 @@ from .C_decoding import ConstrainedDecoding
 from pathlib import Path
 
 if __name__ == "__main__":
+    try:
+        args = ArgParse().get_args
 
-    args = ArgParse().get_args
+        parser = Parser(args)
+        prompt_data = parser.parse_prompt_data
+        funt_data = parser.parse_func_definitions_data
 
-    parser = Parser(args)
-    prompt_data = parser.parse_prompt_data
-    funt_data = parser.parse_func_definitions_data
+        output_path = Path(args.output)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
 
-    output_path = Path(args.output)
-    output_path.parent.mkdir(parents=True, exist_ok=True)
+        result = ConstrainedDecoding(prompt_data, funt_data).json_format()
 
-    result = ConstrainedDecoding(prompt_data, funt_data).json_format()
-
-    with open(output_path, "w") as file:
-        json.dump(json.loads(result), file, indent=4)
+        with open(output_path, "w") as file:
+            json.dump(json.loads(result), file, indent=4)
+    except Exception as e:
+        print(e)
